3c6160436e23e376fab06710425513e1ba2624d8
abel0v <69434823+abel0v@users.noreply.github.com>
2024-04-15 23:39:03 +0400

    Update Main.py
    new

--- Main.py
+++ Main.py
@@ -1,20 +1,29 @@
-import logging
-
 from telebot import TeleBot, types
 
 TELEGRAM_TOKEN = '6512871872:AAEpkj9ZhRUwIW85GfagOhbL05og8MQOioI'
 
 # Инициализация бота Telegram
 bot = TeleBot(TELEGRAM_TOKEN)
+andrtxt = 'Привет, это бот. Сейчас Андрей не может ответить на твое сообщение, но я ему передал, что ты заходил'
+users = [763649163]
 
 
 @bot.business_message_handler(func=lambda message: True, content_types=['text', 'photo', 'video'])
 def handle_business_message(message):
     user_id = message.chat.id
+    chel_id = message.from_user.id
     business_connection_id = message.business_connection_id
     print(message.text)
-    bot.send_message(message.chat.id, 'ddd', reply_to_message_id=message.id,
-                     business_connection_id=business_connection_id)
+    print('chatid', user_id)
+    print('user writing:', chel_id)
+
+    if chel_id not in users:
+        bot.send_message(message.chat.id, andrtxt, reply_to_message_id=message.id,
+                         business_connection_id=business_connection_id)
+        users.append(chel_id)
+    else:
+        pass
+    print(f'in users now: {users}')
 
 
 @bot.message_handler(commands=['start'])
