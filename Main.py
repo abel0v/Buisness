937015baea7b274a1bcd986e5b6e853c12d59468
abel0v <69434823+abel0v@users.noreply.github.com>
2024-04-16 00:24:23 +0400

    Update Main.py
    upd

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ TELEGRAM_TOKEN = '6512871872:AAEpkj9ZhRUwIW85GfagOhbL05og8MQOioI'
 bot = TeleBot(TELEGRAM_TOKEN)
 andrtxt = 'Привет, это бот. Сейчас Андрей не может ответить на твое сообщение, но я ему передал, что ты заходил'
 users = [763649163]
+myid = 763649163
 
 
 @bot.business_message_handler(func=lambda message: True, content_types=['text', 'photo', 'video'])
@@ -17,18 +18,27 @@ def handle_business_message(message):
     print('chatid', user_id)
     print('user writing:', chel_id)
 
-    if chel_id not in users:
+    if chel_id not in users or chel_id != myid:
         bot.send_message(message.chat.id, andrtxt, reply_to_message_id=message.id,
                          business_connection_id=business_connection_id)
+    
+        bot.send_message(myid, f"пишет чел такой tg://user?id={chel_id}: \n {message.text}")
         users.append(chel_id)
     else:
         pass
+    
     print(f'in users now: {users}')
 
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.reply_to(message, "Привет! Я ваш бот.")
+    print(message.text)
+
+
+@bot.message_handler(content_types=['text'])
+def reply(message):
+    bot.send_message(message, "Привет! Я ваш бот, информации не было!")
     print(message.text)
 
 
