1af4c302ffadb0ea94072f06871a885350205c6b
abel0v <69434823+abel0v@users.noreply.github.com>
2024-04-17 23:56:44 +0400

    delete base

--- Main.py
+++ Main.py
@@ -5,8 +5,8 @@ TELEGRAM_TOKEN = '6512871872:AAFhDjesvJ1CU4F8wkyBjOgdQQki4RqAW8o'
 # Инициализация бота Telegram
 bot = TeleBot(TELEGRAM_TOKEN)
 andrtxt = 'Привет, это бот. Сейчас Андрей не может ответить на твое сообщение, но я ему передал, что ты заходил'
+users = [763649163]
 myid = 763649163
-users = open('base.txt', encoding='utf-8')
 
 
 @bot.business_message_handler(func=lambda message: True, content_types=['text', 'photo', 'video'])
@@ -18,19 +18,16 @@ def handle_business_message(message):
     print('chatid', user_id)
     print('user writing:', chel_id)
 
-    if str(chel_id) not in users and chel_id != myid:
+    if chel_id not in users and chel_id != myid:
         bot.send_message(message.chat.id, andrtxt, reply_to_message_id=message.id,
                          business_connection_id=business_connection_id)
-
+    
         bot.send_message(myid, f"пишет чел такой tg://user?id={chel_id}: \n {message.text}")
-        users.write(str(chel_id))
-        users.write(', ')
-
+        users.append(chel_id)
     else:
         pass
-
-
-print(f'in users now: {users.read()}')
+    
+    print(f'in users now: {users}')
 
 
 @bot.message_handler(commands=['start'])
